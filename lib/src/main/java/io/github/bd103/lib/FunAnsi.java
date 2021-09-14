7fda0c9b14007731396e69d574e0384b17cb2977
BD103 <59022059+BD103@users.noreply.github.com>
2021-09-14 11:40:16 -0400

    feat: add BGEX color enum

--- lib/src/main/java/io/github/bd103/lib/FunAnsi.java
+++ lib/src/main/java/io/github/bd103/lib/FunAnsi.java
@@ -9,7 +9,8 @@ public class FunAnsi {
   public static enum Colors {
     BASIC(new int[]{31, 33, 32, 36, 34, 35}),
     BG(new int[]{41, 43, 42, 46, 44, 45}),
-    EX(new int[]{91, 93, 92, 96, 94, 95});
+    EX(new int[]{91, 93, 92, 96, 94, 95}),
+    BGEX(new int[]{101, 103, 102, 106, 104, 105});
 
     public final int[] codes;
 
